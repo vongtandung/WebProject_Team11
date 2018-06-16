58d918f8f908f16cfb3ed6546774868a484ab4a3
Vòng Tần Dũng <32818357+vongtandung@users.noreply.github.com>
2018-06-17 00:04:24 +0700

    Add files via upload

---
+++ app/fn/db.js
@@ -0,0 +1,49 @@
+var mysql = require('mysql');
+
+exports.load = sql => {
+    return new Promise((resolve, reject) => {
+        var cn = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: '',
+            database: 'thesheep'
+        });
+
+        cn.connect();
+
+        cn.query(sql, function(error, rows, fields) {
+            if (error) {
+            	reject(error);
+            } else {
+            	resolve(rows);
+            }
+
+            cn.end();
+        });
+    });
+}
+
+exports.save = sql => {
+    return new Promise((resolve, reject) => {
+        var cn = mysql.createConnection({
+            host: 'localhost',
+            port: 3306,
+            user: 'root',
+            password: '',
+            database: 'thesheep'
+        });
+
+        cn.connect();
+
+        cn.query(sql, function(error, value) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(value);
+            }
+
+            cn.end();
+        });
+    });
+}
