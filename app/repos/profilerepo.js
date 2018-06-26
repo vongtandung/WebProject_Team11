0e85d82ae01a480f83be1c61906c7b76e4140a66
LVDuong <32818804+LVDuong@users.noreply.github.com>
2018-06-26 08:51:03 +0700

    Add files via upload

--- app/repos/profilerepo.js
+++ app/repos/profilerepo.js
@@ -5,36 +5,16 @@ exports.loadallcategories = () => {
 	return db.load(sql);
 }
 
-exports.deletecategory=id=>{
-	var sql = `delete from account where Makh = '${id}'`;
+exports.deletecategory=email=>{
+	var sql = `delete from account where Email = '${email}'`;
 	return db.save(sql);
 }
-exports.editcategoryemail = (email,id) => {
-	var sql = `update account set Email='${email}' where Makh = '${id}'`;
+exports.add = user => {
+	var sql = `INSERT INTO account VALUES('${user.email}', '${user.password}', '${user.name}', '${user.phone}', '${user.dob}','${user.address}', '${user.gender}', ${user.index})`;
 	return db.save(sql);
 }
-exports.editcategoryname = (name,id) => {
-	var sql = `update account set Name='${name}' where Makh = '${id}'`;
-	return db.save(sql);
-}
-exports.editcategoryphone = (phone,id) => {
-	var sql = `update account set Phone='${phone}' where Makh = '${id}'`;
-	return db.save(sql);
-}
-exports.editcategorydob = (dob,id) => {
-	var sql = `update account set DOB='${dob}' where Makh = '${id}'`;
-	return db.save(sql);
-}
-exports.editcategoryaddress = (address,id) => {
-	var sql = `update account set Address= '${address}' where Makh = '${id}'`;
-	return db.save(sql);
-}
-exports.editcategorygender = (gender,id) => {
-	var sql = `update account set Gender='${gender}' where Makh = '${id}'`;
-	return db.save(sql);
+exports.findemail=email=>{
+	var sql = `select * from account where Email='${email}'`;
+	return db.load(sql);
 }
 
-exports.add = category => {
-	var sql = `insert into account(Makh, Name, Email, Phone, DOB, Address, Gender) values('${category.id}', '${category.name}', '${category.email}', '${category.phone}',  '${category.dob}','${category.address}','${category.gender}')`;
-	return db.save(sql);
-}
